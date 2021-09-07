0d303e233bafd34257fa6f2867fc2f1cc76f44d9
Maksym Churkin <imaximius@gmail.com>
2021-09-07 17:58:59 +0300

    Update Converter.php

--- Mapping/Converter.php
+++ Mapping/Converter.php
@@ -47,6 +47,10 @@ class Converter
 
     protected function normalize($document, $metadata = null)
     {
+        if(empty($document)) {
+            return ;
+        }
+        
         $metadata = $metadata ?? $this->propertyMetadata[get_class($document)];
         $result = [];
 
